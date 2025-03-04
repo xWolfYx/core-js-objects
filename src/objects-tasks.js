008661dc3681c32afae642e83478177db0dd155f
xWolfYx <david.skliarov@gmail.com>
2025-03-04 17:46:11 +0400

    feat: resolve mergeObjects function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -33,8 +33,15 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  return objects.reduce((result, obj) => {
+    const newObj = { ...result };
+
+    Object.entries(obj).forEach(([key, value]) => {
+      newObj[key] = (newObj[key] || 0) + value;
+    });
+    return newObj;
+  }, {});
 }
 
 /**
